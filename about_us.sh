733f158cccb296637a5c6cc5b893a56ab930fb8a
Rifky KD <mohammedrifky5431@gmail.com>
2021-09-28 10:38:47 +0530

    Update about_us.sh

--- about_us.sh
+++ about_us.sh
@@ -7,7 +7,15 @@ sleep 2
 sleep 1
 echo "                      GitHub : github.com/rifkykd" |lolcat
 sleep 1
-echo -e "[ ✔ ] HAPPY HACKING ..."
-echo -e "[ ✔ ] ALWAYS DOING SOMTHING IS BETTER THAN DOING NOTHING"
+echo "                      Insta  : https://www.instagram.com/rifky__kd/" |lolcat
+sleep 1
+echo "                      TikTok : https://www.tiktok.com/@rifky_kd?lang=en" |lolcat
+sleep 1
+echo "                      Twitter: https://twitter.com/Rifky54641898" |lolcat
+sleep 1
+echo "                      Youtube: Rifky KD" |lolcat
+sleep 1
+echo -e "                       [ ✔ ] HAPPY HACKING ..."
+echo -e "          [ ✔ ] ALWAYS DOING SOMTHING IS BETTER THAN DOING NOTHING"
 sleep 3
 bash main_script.sh
